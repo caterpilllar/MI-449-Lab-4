584302d3264dda060f569089725ec8784a5d7c41
caterpilllar <123208987+caterpilllar@users.noreply.github.com>
2024-02-10 17:02:10 -0500

    done

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,19 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+
+  eleventyConfig.addShortcode("card", function(planetName, au){
+    `<div class="card ${planetName}">
+    <div class="planet ${planetName}y"></div>
+    <h3> ${planetName} </h3>
+    <p> AU: ${au} </p> 
+  </div>`
+  });
+  
+
+  eleventyConfig.addShortcode("nav", function(title){
+    return `<h1> ${title} </h1`
+  });
+
+
 };
